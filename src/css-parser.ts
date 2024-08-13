52578c2f74f93214f4ef5608406911eb9ac296c3
GaoNeng-wWw <gaonenggg@outlook.com>
2024-08-13 21:44:42 +0800

    feat: inline layout node

--- src/css-parser.ts
+++ src/css-parser.ts
@@ -1,25 +1,58 @@
 import antlr, {ParseTreeWalker} from 'antlr4';
 import Lexer from './cssParser/css3Lexer';
-import Parser, { DeclarationContext, DeclarationListContext, SimpleSelectorSequenceContext } from './cssParser/css3Parser';
+import Parser, { BadPropertyContext, DeclarationContext, DeclarationListContext, DimensionContext, ExprContext, ExpressionContext, GoodCharsetContext, GoodPropertyContext, KnownDeclarationContext, SimpleSelectorSequenceContext, UnknownDeclarationContext } from './cssParser/css3Parser';
 import css3ParserListener from './cssParser/css3ParserListener';
 
 
 class Walker extends css3ParserListener {
   private name!: string;
-  private record: Map<string, DeclarationContext[]> = new Map();
+  private proerty!: string;
+  private expr!: ExprContext;
+  private record: Map<string, Record<string, ExprContext>> = new Map();
   exitSimpleSelectorSequence?: ((ctx: SimpleSelectorSequenceContext) => void) | undefined = (ctx) => {
     this.name = ctx.getText();
     if (!this.record.has(this.name)){
-      this.record.set(this.name, []);
+      this.record.set(this.name, {});
     }
   }
   exitDeclarationList?: ((ctx: DeclarationListContext) => void) | undefined = (ctx)=>{
-    const decls = ctx.declaration_list();
-    this.record.set(this.name, [
-      ...this.record.get(this.name) ?? [],
-      ...decls
-    ])
     this.name = '';
+  }
+  exitBadProperty?: ((ctx: BadPropertyContext) => void) | undefined = (ctx) => this.proerty = ctx.getText();
+  exitGoodProperty?: ((ctx: GoodPropertyContext) => void) | undefined = (ctx) => this.proerty = ctx.getText();
+
+  exitExpr?: ((ctx: ExprContext) => void) | undefined = (ctx) => this.expr = ctx;
+  exitKnownDeclaration?: ((ctx: KnownDeclarationContext) => void) | undefined = (ctx) => {
+    if (this.record.get(this.name)) {
+      this.record.set(
+        this.name,
+        {
+          ...this.record.get(this.name),
+          [this.proerty]: this.expr
+        }
+      );
+      return;
+    }
+    this.record.set(this.name, {
+      [this.proerty]: this.expr
+    })
+    return;
+  }
+  exitUnknownDeclaration?: ((ctx: UnknownDeclarationContext) => void) | undefined = (ctx) => {
+    if (this.record.get(this.name)) {
+      this.record.set(
+        this.name,
+        {
+          ...this.record.get(this.name),
+          [this.proerty]: this.expr
+        }
+      );
+      return;
+    }
+    this.record.set(this.name, {
+      [this.proerty]: this.expr
+    })
+    return;
   }
   get tree(){
     return this.record;
@@ -27,7 +60,7 @@ class Walker extends css3ParserListener {
 }
 
 export class CSSOM {
-  public tree: Map<string, DeclarationContext[]> = new Map();
+  public tree: Map<string, Record<string, ExprContext>> = new Map();
   constructor(
     css:string
   ){

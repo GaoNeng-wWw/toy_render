e1dc362ea732c63f5cc404a361cd8d8f0aee7201
GaoNeng-wWw <gaonenggg@outlook.com>
2024-08-16 20:08:08 +0800

    feat(layout): inline layout and paint

--- src/layout-tree.ts
+++ src/layout-tree.ts
@@ -29,7 +29,7 @@ export class Layout{
       h:this.height
     }
   }
-  pain(ctx: CanvasRenderingContext2D): void{}
+  paint(ctx: CanvasRenderingContext2D): void{}
 }
 export class InlineLayout extends Layout{
   constructor(
@@ -51,20 +51,22 @@ export class InlineLayout extends Layout{
     this.parent=parent;
   }
   layout(ctx: CanvasRenderingContext2D){
-    const childrenLayoutInfo = this.children.map((child) => child.layout(ctx));
-    const width = childrenLayoutInfo.reduce((pre,cur) => {
-      return pre + cur.w
-    }, 0);
-    const height = Math.max(...childrenLayoutInfo.map((child) => child.h));
+    for (const child of this.children){
+      const info = child.layout(ctx);
+      this.width += info.w;
+      this.height = Math.max(this.height, info.h);
+    }
+    this.x = this.parent?.width ?? 0;
     return {
       x:this.x,
       y:this.y,
-      w:width,
-      h:height
+      w:this.width,
+      h:this.height
     }
   }
-  pain(ctx: CanvasRenderingContext2D): void {
-    this.children.forEach(child => child.pain(ctx));
+  paint(ctx: CanvasRenderingContext2D): void {
+    super.paint(ctx);
+    this.children.forEach(child => child.paint(ctx));
   }
 }
 
@@ -78,19 +80,19 @@ export class TextLayout extends InlineLayout {
     this.data = data
   }
   layout(ctx: CanvasRenderingContext2D): { x: number; y: number; w: number; h: number; } {
-    const {width ,fontBoundingBoxDescent} = ctx.measureText(this.data);
-    const x = this.parent?.width ?? 0 + width;
-    const y = 0;
+    const {width ,fontBoundingBoxAscent} = ctx.measureText(this.data);
+    const x = this.parent?.width ?? 0;
+    const y = fontBoundingBoxAscent;
     this.x=x;
     this.y=y;
     this.width=width;
-    this.height=fontBoundingBoxDescent;
+    this.height=fontBoundingBoxAscent;
     return {
       x,y,
-      w: width, h:fontBoundingBoxDescent
+      w: width, h:this.height
     }
   }
-  pain(ctx: CanvasRenderingContext2D): void {
-    ctx.fillText(this.data, this.x, this.y, this.width);
+  paint(ctx: CanvasRenderingContext2D): void {
+    ctx.fillText(this.data, this.x, this.y);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,29 @@
-const cavnas = document.querySelector('#canvas')! as HTMLCanvasElement;
-const ctx = cavnas.getContext('2d')!;
+import { InlineLayout, TextLayout } from "./layout-tree";
+
+const canvas = document.querySelector('#canvas')! as HTMLCanvasElement;
+const ctx = canvas.getContext('2d')!;
+let width = 200;
+let height = 200;
+const observer = new ResizeObserver(() => {
+  width = canvas.clientWidth;
+  height = canvas.clientHeight;
+});
+observer.observe(canvas)
+
+function start(){
+  canvas.width = width;
+  canvas.height = height;
+
+
+  const inline = new InlineLayout(0,0,0,0,[],null);
+  const cn = new TextLayout(inline, '你好 ');
+  const en = new TextLayout(inline, 'hello world')
+  inline.children.push(cn,en);
+  inline.layout(ctx);
+  inline.paint(ctx);
+  requestAnimationFrame(start)
+}
+
+// start();
+
+requestAnimationFrame(start)
